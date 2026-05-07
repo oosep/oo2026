3f37508f3a3af0ee8abff140860f74c057467e86
oosep <oosep@tlu.ee>
2026-05-08 01:26:41 +0300

    Add files via upload

---
+++ 01_06.02-andmemudelid/veebipood/src/main/java/ee/joosep/veebipood/entity/Film.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String director;
+    private int year;
+}
