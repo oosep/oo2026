3c8e17e5a868347bac81ea8f1f58b891f21729a1
oosep <oosep@tlu.ee>
2026-05-15 07:43:58 +0300

    Delete 02_13.02-veateated/autod directory

--- 02_13.02-veateated/autod/src/main/java/repository/AutoRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.joosep.autod.repository;
-
-import ee.joosep.autod.entity.Auto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutoRepository extends JpaRepository<Auto, Long> {
-}
