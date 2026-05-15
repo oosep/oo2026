6802f6aea0e0b9dfa739449a8ba040cb54ac1136
oosep <oosep@tlu.ee>
2026-05-15 07:35:25 +0300

    Add files via upload

---
+++ 01_06.02-andmemudelid/veebipood/src/main/java/ee/joosep/veebipood/controller/FilmController.java
@@ -0,0 +1,33 @@
+package ee.joosep.veebipood.controller;
+
+import ee.joosep.veebipood.entity.Film;
+import ee.joosep.veebipood.repository.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("filmid")
+public class FilmController {
+
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return filmRepository.findAll();
+    }
+
+    @PostMapping
+    public List<Film> addFilm(@RequestBody Film film) {
+        filmRepository.save(film);
+        return filmRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Film> deleteFilm(@PathVariable Long id) {
+        filmRepository.deleteById(id);
+        return filmRepository.findAll();
+    }
+}
